5c2b8dae474095b50576a26fce4fe9e1dd9881fe
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-12 12:22:39 +0545

    get Literature API

--- server/controller/literature.controller.js
+++ server/controller/literature.controller.js
@@ -11,5 +11,12 @@ export const shareLiterature = async (req, res, next) => {
       .json({ message: "Literature validation failed", error: error.message });
   }
 };
-
-export const getLiteratures = async (req, res, next) => {};
+export const getLiteratures = async (req, res, next) => {
+  try {
+    // Fetch only literatures with approvalStatus set to true
+    const literatures = await Literature.find({ approvalStatus: true });
+    res.status(200).json(literatures);
+  } catch (error) {
+    res.status(500).json({ error: "Failed to fetch literature data" });
+  }
+};
