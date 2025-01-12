882182e623a22721f67433827fb6cbdd6909ab9b
Sonoop <sonupwannabe@gmail.com>
2025-01-12 13:26:50 +0545

    Additional components

--- client/src/component/Index.js
+++ client/src/component/Index.js
@@ -6,8 +6,11 @@ import Profile from "./Profile.jsx";
 import Share from "./Share.jsx";
 import Header from "./Header.jsx";
 import Footer from "./Footer.jsx";
+import Login from "./Login.jsx";
+import Signup from "./Signup.jsx";
+import Explore from "./Explore.jsx";
 
 
 
 
-export {Logo,Approve,Feed,Home,Profile,Share,Header,Footer};
+export {Logo,Approve,Feed,Home,Profile,Share,Header,Footer, Login, Signup, Explore};
