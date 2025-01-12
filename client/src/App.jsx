bbbecd04fee848d556a4823e4e98fb2174107200
narayan <you@example.com>
2025-01-12 05:21:28 -0800

    header updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,21 @@ import "./App.css";
 function App() {
   return (
     <div className="min-h-screen flex flex-col bg-[#F5F0E1] text-[#4E342E] font-serif">
-      {/* Header */}
-      <Header />
+      {/* Fixed Header */}
+      <div className="fixed top-0 left-0 w-full z-10">
+        <Header />
+      </div>
 
-      {/* Main Content */}
-      <div className="flex flex-grow">
-        {/* LCard on the left */}
-        <div className="w-1/4 bg-gray-100 shadow-md h-full p-6">
+      {/* Main Content with padding-top to prevent overlap */}
+      <div className="flex flex-grow pt-[80px]">
+        {" "}
+        {/* Padding top equal to the height of the header */}
+        {/* LCard on the left (Sidebar) */}
+        <div className="w-full md:w-1/4 bg-gray-100 shadow-md p-6">
           <LCard />
         </div>
-
         {/* Dynamic Content (Outlet) */}
-        <main className="w-3/4 p-6 overflow-y-auto">
+        <main className="w-full md:w-3/4 p-6 overflow-y-auto">
           <Outlet />
         </main>
       </div>
