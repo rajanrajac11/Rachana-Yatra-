57a479d3ff792d7e3eb71a63d7c85a60200bb3ca
narayan <you@example.com>
2025-01-12 02:45:59 -0800

    feed changed

--- client/src/component/FeedCard.jsx
+++ client/src/component/FeedCard.jsx
@@ -10,12 +10,12 @@ function FeedCard({
   return (
     <div className="w-[60%] mx-auto p-6 bg-[#F5F0E1] border border-[#DCCFC0] rounded-xl shadow-lg hover:shadow-2xl transition duration-300 flex flex-col items-center text-center relative">
       {/* Author - Positioned at the top-left */}
-      <p className="text-sm text-[#6B4226] font-semibold absolute top-4 left-4">
+      <p className="text-md text-[#6B4226] font-semibold absolute top-4 left-4">
         Author: {author}
       </p>
 
       {/* Published Date */}
-      <p className="text-sm text-[#8E7C3D] italic absolute top-4 right-4">
+      <p className="text-md text-[#8E7C3D] italic absolute top-4 right-4">
         <span className="font-semibold">Published On:</span>{" "}
         {new Date(createdAt).toLocaleDateString()}
       </p>
@@ -29,7 +29,7 @@ function FeedCard({
       <h2 className="text-3xl font-bold text-[#4E342E] mb-4">{title}</h2>
 
       {/* Content */}
-      <div className="bg-[#FFF9F1] px-4 py-6 rounded-lg shadow-md border border-[#EADBCB] text-left">
+      <div className="bg-[#FFF9F1] px-4 py-6 rounded-lg shadow-md border border-[#EADBCB] text-left w-1/2">
         {content.split("\n").map((paragraph, index) => (
           <p
             key={index}
