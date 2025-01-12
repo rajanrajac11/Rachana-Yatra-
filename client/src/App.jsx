baea0211a65018bc00476ed3ea7781cbf2836d82
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-12 15:21:36 +0545

    app changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,25 +1,21 @@
-import { Header, Footer } from "./component/Index.js";
+import { useState } from "react";
+import { Header, Footer } from "./component/Index";
+
 import { Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
     <div className="min-h-screen flex flex-col bg-[#F5F0E1] text-[#4E342E] font-serif">
       {/* Header */}
       <Header />
 
-      {/* Main Content Area */}
-      <div className="flex flex-1">
-        {/* Left Fixed Card */}
-        <div className="w-1/3 bg-gray-100 shadow-md fixed h-full p-6 text-center">
-          {/* <Lcard /> */}
-        </div>
-
-        {/* Dynamic Content with Outlet */}
-        <main className="ml-1/3 w-2/3 p-6 overflow-y-auto">
-          <Outlet />
-        </main>
-      </div>
+      {/* Main Content */}
+      <main className="flex-grow p-2">
+        <Outlet />
+      </main>
 
       {/* Footer */}
       <Footer />
