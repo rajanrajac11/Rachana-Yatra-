598d1631945dfbdc8ec8f87504e0b736709c1b1c
Sonoop <sonupwannabe@gmail.com>
2025-01-12 13:26:13 +0545

    routing

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,65 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  Home,
+  Feed,
+  Explore,
+  Login,
+  Signup,
+  Share,
+  Approve,
+} from "./component/Index.js";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/feed",
+        element: <Feed />,
+      },
+      {
+        path: "/share-literature",
+        element: <Share/>,
+      },
+      {
+        path: "/explore",
+        element: <Explore />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+      {
+        path: "/signup",
+        element: <Signup />,
+      },
+      // {
+      //   path: "/profile",
+      //   element: (
+      //     <PrivateRoute>
+      //       <Profile />
+      //     </PrivateRoute>
+      //   ),
+      // },
+      {
+        path: "/approval",
+        element: <Approve />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
+    <StrictMode>
+      <RouterProvider router={router}></RouterProvider>
+    </StrictMode>
+);
