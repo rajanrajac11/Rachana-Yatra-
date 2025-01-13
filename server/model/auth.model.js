e6b242541697a90a0bc65fc84a858c96705f3d23
narayan <you@example.com>
2025-01-13 08:56:58 +0545

    auth-model/schema

---
+++ server/model/auth.model.js
@@ -0,0 +1,36 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    adminStatus: {
+      type: Boolean,
+      default: false,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        "https://thumbs.dreamstime.com/b/default-avatar-profile-icon-vector-social-media-user-image-182145777.jpg",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+const User = mongoose.model("User", userSchema);
+
+export default User;
