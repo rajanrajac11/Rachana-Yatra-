d10183099d01cfb9dc9f320bafaac764591b7b59
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-13 09:05:42 +0545

    reject-and-accept-api

--- server/controller/literature.controller.js
+++ server/controller/literature.controller.js
@@ -3,7 +3,6 @@ import Literature from "../model/literature.model.js";
 export const shareLiterature = async (req, res, next) => {
   try {
     const newLiterature = await Literature.create(req.body);
-    console.log(req.body);
     res.status(200).json({ message: "Literature created successfully" });
   } catch (error) {
     console.log("Validation Error: ", error.message);
@@ -26,6 +25,56 @@ export const getNotApprovedLiteratures = async (req, res, next) => {
     // Fetch only literatures with approvalStatus set to true
     const literatures = await Literature.find({ approvalStatus: false });
     res.status(200).json(literatures);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const rejectLiterature = async (req, res, next) => {
+  try {
+    //getting id from the request body
+    const { id } = req.body;
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "ID is required to delete" });
+    }
+    const deletedLiterature = await Literature.findByIdAndDelete(id);
+
+    if (!deletedLiterature) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Literature not found" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Literature deleted successfully",
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const acceptLiterature = async (req, res, next) => {
+  try {
+    const { id } = req.body;
+    if (!id) {
+      return res
+        .status(400)
+        .json({ success: false, message: "ID is required to approve" });
+    }
+    const acceptedLiterature = await Literature.findByIdAndUpdate(id, {
+      approvalStatus: true,
+    });
+    if (!acceptedLiterature) {
+      return res
+        .status(404)
+        .json({ success: false, message: "Literature not found" });
+    }
+    res.status(200).json({
+      success: true,
+      message: "Literature approved Successfully",
+    });
   } catch (error) {
     next(error);
   }
