45701ea4bf082b14204544337e0c35c4344cfc28
narayan <you@example.com>
2025-01-13 08:48:31 +0545

    auth-controller-server

---
+++ server/controller/auth.controller.js
@@ -0,0 +1,44 @@
+import User from "../model/auth.model.js";
+import bcryptjs from "bcryptjs";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
+
+export const signup = async (req, res, next) => {
+  try {
+    const { username, email, password } = req.body;
+    const hashedPassword = bcryptjs.hashSync(password, 13);
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(200).json("User created successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+export const login = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+    const validUser = await User.findOne({ email });
+    if (!validUser) return next(errorHandler(404, "User not found"));
+
+    const validPassword = bcryptjs.compareSync(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Invalid credentials"));
+
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: hashedPassword, ...rest } = validUser._doc;
+
+    const expiryDate = new Date(Date.now() + 3600000);
+    res
+      .cookie("access_token", token, { httpOnly: true, expires: expiryDate })
+      .status(200)
+      .json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signout = async (req, res) => {
+  res.clearCookie("access_token").status(200).json("Signout Success");
+};
