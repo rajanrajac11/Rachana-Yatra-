6c8bb26c9d3e5b33b3309491a549d733af2f9022
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-14 17:29:36 +0545

    add: custom-api-fetch-hook-added

---
+++ client/src/customApiHook/useFetch.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+export function useFetch(url) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const response = await axios.get(url);
+        setData(response.data);
+      } catch (err) {
+        setError(err.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return { data, error, loading };
+}
