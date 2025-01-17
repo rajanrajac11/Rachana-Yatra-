a4b25d34587b77136b78875b2226edb81346fe22
Rajan <acharya.rajan.raj11@gmail.com>
2025-01-17 23:31:44 +0545

    update: Custom useFetch to handle other req too

--- client/src/customApiHook/useFetch.js
+++ client/src/customApiHook/useFetch.js
@@ -1,25 +1,34 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-export function useFetch(url) {
+
+export function useFetch(url, method = "GET", body = null) {
   const [data, setData] = useState([]);
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
 
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    try {
+      const options = {
+        method,
+        url,
+        data: body,
+      };
+
+      const response = await axios(options);
+      setData(response.data);
+    } catch (err) {
+      setError(err.message);
+    } finally {
+      setLoading(false);
+    }
+  }, [url, method, body]);
+
   useEffect(() => {
-    const fetchData = async () => {
-      setLoading(true);
-      try {
-        const response = await axios.get(url);
-        setData(response.data);
-      } catch (err) {
-        setError(err.message);
-      } finally {
-        setLoading(false);
-      }
-    };
+    if (method === "GET") {
+      fetchData();
+    }
+  }, [fetchData, method]);
 
-    fetchData();
-  }, [url]);
-
-  return { data, error, loading };
+  return { data, error, loading, fetchData };
 }
